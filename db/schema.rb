9b03ee779a00aa0150c76a44356d5830c5bc3d5d
BiancaMe <bianquis0410@gmail.com>
2023-11-14 23:11:06 -0300

    Add posts schema

--- db/schema.rb
+++ db/schema.rb
@@ -40,4 +40,21 @@ ActiveRecord::Schema[7.1].define(version: 0) do
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "comments_counter"
+    t.integer "likes_counter"
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
