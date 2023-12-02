4858357f8dbd8b800e609c735500bb7cc6af0b12
BiancaMe <bianquis0410@gmail.com>
2023-12-02 17:23:48 -0300

    Add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,28 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id].to_i)
+    @posts = Post.where(author_id: @user.id)
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render 'new'
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
